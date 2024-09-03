94f15eef569a06e94d7b2b3115f7cd69099595ec
Jeremy Panjaitan <jeremy.panjaitan@nanovest.io>
2024-09-03 10:27:00 +0700

    Menambahkan nama

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
     //sout
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Jeremy Panjaitan");
     }
 }
