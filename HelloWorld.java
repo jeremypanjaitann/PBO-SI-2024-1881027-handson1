2f95edb4f71adad0148abe681ff3c9d2a9f0a56f
jeremypanjaitann <jeremy.panjaitan@unai.edu>
2024-10-10 08:35:32 +0700

    Added feature a

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Jeremy Panjaitan");
+        System.out.println("Perubahan dibikin oleh Jeremy Panjaitan");
     }
 }
